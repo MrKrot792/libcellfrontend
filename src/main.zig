7a03c680f189711622143ceabf6063de7fb16c2b
Vladislav Korotianets <vkorotanec@gmail.com>
2025-11-30 19:16:15 +0100

    Added an allocator to `main.zig` (not for the release, and this won't work... for now)

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,17 @@ const std = @import("std");
 const libcellfrontend = @import("libcellfrontend");
 
 pub fn main() !void {
-    libcellfrontend.init("cool window", .{800, 500});
-    libcellfrontend.loop();
-    libcellfrontend.deinit();
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer {
+        switch (debug_allocator.deinit()) {
+            .leak => std.log.debug("You leaked memory dum dum", .{}),
+            .ok => std.log.debug("No memory leaks. For now...", .{}),
+        }
+    }
+
+    const allocator = debug_allocator.allocator();
+
+    try libcellfrontend.init("cool window", .{800, 500}, allocator);
+    try libcellfrontend.loop(allocator);
+    libcellfrontend.deinit(allocator);
 }
