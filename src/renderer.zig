4d490612c2a5c45ec261d6c3f851f10f90a7bfdc
Vladislav Korotianets <vkorotanec@gmail.com>
2025-12-01 15:34:46 +0100

    Added a simple camera, it's controlled with `wasd` and `jk`.

--- src/renderer.zig
+++ src/renderer.zig
@@ -5,6 +5,15 @@ const clay = @import("zclay");
 
 var image: rlz.Image = undefined;
 var texture: rlz.Texture = undefined;
+var renderTexture: rlz.RenderTexture2D = undefined;
+
+var camera: rlz.Camera2D = .{ 
+    .offset = .{ .x = 0, .y = 0 }, // not .zero because it's not comptime
+    .rotation = 0, 
+    .target = .{ .x = 0, .y = 0 }, 
+    .zoom = 1 
+};
+
 
 pub fn init(name: [:0]const u8, size: [2]i32) !void {
     rlz.setConfigFlags(.{ .window_resizable = true });
@@ -15,18 +24,38 @@ pub fn init(name: [:0]const u8, size: [2]i32) !void {
     rc.raylib_fonts[0] = try rlz.loadFontEx("assets/fonts/HackNerdFontMono-Regular.ttf", 32, null); // this is probably UI? i'm not sure tho
     rlz.setTextureFilter(rc.raylib_fonts[0].?.texture, .bilinear);
 
-    image = .genGradientLinear(10, 10, 0, .red, .blue);
+    image = .genCellular(500, 500, 50);
     texture = try image.toTexture();
+    renderTexture = try .init(1, 1);
 }
 
 pub fn draw(where: clay.BoundingBox) !void {
     rlz.updateTexture(texture, image.data);
+
     if (rlz.isWindowResized()) {
-        image = .genGradientLinear(@intFromFloat(where.width), @intFromFloat(where.height), 0, .red, .blue);
-        texture = try .fromImage(image);
+        renderTexture = try .init(@intFromFloat(where.width), @intFromFloat(where.height));
+        camera.offset = .init(where.width / 2.0, where.height / 2.0);
     }
 
-    texture.draw(@intFromFloat(where.x), @intFromFloat(where.y), .white);
+    const speed:      f32 = 0.8;
+    const zoom_speed: f32 = 0.005;
+
+    if (rlz.isKeyDown(.w)) camera.target.y += speed / camera.zoom;
+    if (rlz.isKeyDown(.a)) camera.target.x -= speed / camera.zoom;
+    if (rlz.isKeyDown(.s)) camera.target.y -= speed / camera.zoom;
+    if (rlz.isKeyDown(.d)) camera.target.x += speed / camera.zoom;
+
+    if (rlz.isKeyDown(.j)) camera.zoom += zoom_speed * camera.zoom;
+    if (rlz.isKeyDown(.k)) camera.zoom -= zoom_speed * camera.zoom;
+
+    renderTexture.begin();
+    camera.begin();
+    rlz.clearBackground(.black);
+    texture.draw(0, 0, .white);
+    camera.end();
+    renderTexture.end();
+
+    renderTexture.texture.draw(@intFromFloat(where.x), @intFromFloat(where.y), .white);
 }
 
 pub fn deinit() void {
